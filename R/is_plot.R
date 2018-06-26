5552bdf0ddfaef85048c44f2778d99ff2097b166
Lucy <ld.mcgowan@vanderbilt.edu>
2018-06-26 13:46:32 -0500

    :bar_chart: is_plot function

---
+++ R/is_plot.R
@@ -0,0 +1,35 @@
+#' Check if an expression is calling a plot function
+#'
+#' @param x an R expression
+#'
+#' @return logical
+#' @export
+#'
+#' @examples
+#' expr <- matahari::dance_tbl()$expr
+#' purrr::map_lgl(expr, is_plot)
+
+is_plot <- function(x) {
+  if (class(x) %in% c("call", "<-")) {
+    x <- pryr::fun_calls(x)
+    return(
+      any(x %in%
+            c("plot", "abline", "arrows", "asp",
+              "assocplot", "Axis", "axis",
+              "axTicks", "barplot", "bxp", "cdplot", "clip",
+              "co.intervals", "contour", "coplot", "curve",
+              "dotchart", "filled.contour", "fourfoldplot",
+              "frame", "grconvertX", "grconvertY", "grid",
+              "hist", "identify", "image", "lcm",
+              "legend", "lines", "locator", "matlines", "matplot",
+              "matpoints", "mosaicplot", "mtext", "pairs", "panel.smooth",
+              "par", "pch", "points", "polygon", "polypath", "rasterImage",
+              "rect", "rug", "segments", "smoothScatter", "spineplot",
+              "stars", "stem", "strheight", "stripchart", "strwidth",
+              "sunflowerplot", "symbols", "text", "title", "xinch", "xlim",
+              "xspline", "xyinch", "yinch", "ylim",
+              "ggplot"))
+    )
+  } else FALSE
+}
+
