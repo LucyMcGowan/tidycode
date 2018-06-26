d11d8b44affe801fbe8abfb38b11b5d3dd8184f8
Lucy <ld.mcgowan@vanderbilt.edu>
2018-06-26 16:22:52 -0500

    Add more model classes

--- R/is_model.R
+++ R/is_model.R
@@ -10,7 +10,9 @@
 #' purrr::map_lgl(val, is_model)
 
 is_model <- function(x) {
-  any(class(x) %in% c("lm", "glm", "train", "recipe", "lme4", "coxph"))
+  any(class(x) %in% c("lm", "glm", "train", "recipe", "lme4",
+                      "coxph", "rpart", "nnet", "glmnet", "gbm",
+                      "lars", "randomForest"))
 }
 
 
