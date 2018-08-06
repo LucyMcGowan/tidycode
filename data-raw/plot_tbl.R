1cf69adf1abec5946ea522a1bcced8f6d8ae16ad
Lucy McGowan <lucydagostino@gmail.com>
2018-08-06 09:20:17 -0500

    Switch to calling all functions in graphics and ggplot2

--- data-raw/plot_tbl.R
+++ data-raw/plot_tbl.R
@@ -1,22 +1,9 @@
 library(tidyverse)
 library(rprojroot)
-
+library(ggplot2)
 df <- tibble(
-  plot_fx = c("plot", "abline", "arrows", "asp",
-  "assocplot", "Axis", "axis",
-  "axTicks", "barplot", "bxp", "cdplot", "clip",
-  "co.intervals", "contour", "coplot", "curve",
-  "dotchart", "filled.contour", "fourfoldplot",
-  "frame", "grconvertX", "grconvertY", "grid",
-  "hist", "identify", "image", "lcm",
-  "legend", "lines", "locator", "matlines", "matplot",
-  "matpoints", "mosaicplot", "mtext", "pairs", "panel.smooth",
-  "par", "pch", "points", "polygon", "polypath", "rasterImage",
-  "rect", "rug", "segments", "smoothScatter", "spineplot",
-  "stars", "stem", "strheight", "stripchart", "strwidth",
-  "sunflowerplot", "symbols", "text", "title", "xinch", "xlim",
-  "xspline", "xyinch", "yinch", "ylim",
-  "ggplot", "bmp", "jpeg", "png", "tiff")
+  plot_fx = c(ls("package:graphics"),
+              ls("package:ggplot2"))
 )
 
 write_csv(df, path = find_package_root_file("inst", "extdata", "plot_tbl.csv"))
