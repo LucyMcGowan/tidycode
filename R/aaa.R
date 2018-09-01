5f54b6744f9563341db1aff3d6df2e51a8b8ff8d
Lucy McGowan <lucydagostino@gmail.com>
2018-09-01 08:45:05 -0500

    Add function to create :package: tibble

--- R/aaa.R
+++ R/aaa.R
@@ -19,3 +19,9 @@ NULL
   system.file("extdata", "model_tbl.csv", package = "tidycode") %>%
   read.csv(stringsAsFactors = FALSE) %>%
   tibble::as_tibble()
+
+.tidycode$cran_tbl <-
+  system.file("extdata", "namespace_tbl_1000.csv", package = "tidycode") %>%
+  read.csv(stringsAsFactors = FALSE) %>%
+  tibble::as_tibble()
+
---
+++ R/get_package_tbl.R
@@ -0,0 +1,12 @@
+#' Create a tibble of all functions included in all packages used in your analysis
+#'
+#' @param pkg_names Character vector of the names of all packages used in analysis
+#'
+#' @return Tibble with two columns, `package` and `func`
+#' @export
+get_packages_tbl <- function(pkg_names) {
+  pkg_names <- c(pkg_names, "stats", "methods", "grDevices", "graphics",
+                 "datasets", "base")
+  .tidycode$cran_tbl[(.tidycode$cran_tbl$package %in% pkg_names) &
+                       !is.na(.tidycode$cran_tbl$func), ]
+}
