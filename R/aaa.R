68edc8bb85f2ca453121bb49f95503719fbc7dcf
Lucy McGowan <lucydagostino@gmail.com>
2019-05-07 18:51:39 -0400

    Update package internals

--- R/aaa.R
+++ R/aaa.R
@@ -10,16 +10,6 @@ NULL
 
 .tidycode <- new.env(parent = emptyenv())
 
-.tidycode$plot_tbl <-
-  system.file("extdata", "plot_tbl.csv", package = "tidycode") %>%
-  read.csv(stringsAsFactors = FALSE) %>%
-  tibble::as_tibble()
-
-.tidycode$model_tbl <-
-  system.file("extdata", "model_tbl.csv", package = "tidycode") %>%
-  read.csv(stringsAsFactors = FALSE) %>%
-  tibble::as_tibble()
-
 .tidycode$cran_tbl <-
   system.file("extdata", "namespace_tbl_1000.csv", package = "tidycode") %>%
   read.csv(stringsAsFactors = FALSE) %>%
