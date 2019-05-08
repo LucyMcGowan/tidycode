2ff27c47eb8eaf696ff183e6471e94faf03d3442
Lucy McGowan <lucydagostino@gmail.com>
2019-05-08 16:37:43 -0400

    Move large df to internal

--- R/aaa.R
+++ R/aaa.R
@@ -10,11 +10,6 @@ NULL
 
 .tidycode <- new.env(parent = emptyenv())
 
-.tidycode$cran_tbl <-
-  system.file("extdata", "namespace_tbl_1000.csv", package = "tidycode") %>%
-  read.csv(stringsAsFactors = FALSE) %>%
-  tibble::as_tibble()
-
 .tidycode$classification_tbl <-
   system.file("extdata", "classification_tbl.csv", package = "tidycode") %>%
   read.csv(stringsAsFactors = FALSE) %>%
