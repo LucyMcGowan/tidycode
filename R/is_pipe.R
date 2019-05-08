a31fe650b20c987ba8f8e752aac57455958c6521
Lucy McGowan <lucydagostino@gmail.com>
2019-05-08 12:52:56 -0400

    is pipe now internal

--- R/is_pipe.R
+++ R/is_pipe.R
@@ -3,10 +3,6 @@
 #' @param x an R call
 #'
 #' @return logical
-#' @export
-#'
-#' @examples
-#' is_pipe(quote(mtcars$mpg %>% mean()))
 is_pipe <- function(x) {
   if(is.list(x) | (is.character(x) & length(x) > 1)) {
     return(purrr::map_lgl(x, is_pipe))
