5811c02c99953aca0e237a46e79422c1d0bcfb56
Lucy McGowan <lucydagostino@gmail.com>
2019-05-10 16:40:09 -0400

    Allow for URLS

--- R/read_rfiles.R
+++ R/read_rfiles.R
@@ -16,6 +16,25 @@
 read_rfiles <- function(...) {
 
   files <- list(...)
+  urls <- files[purrr::map_lgl(files, inherits, "connection")]
+
+  if (!is.null(urls)) {
+    url_strings <- purrr::map(urls, get_url_string)
+    url_files <- purrr::map(urls, handle_url)
+  }
+
+  file_strings <- files
+  file_strings[purrr::map_lgl(files, inherits, "connection")] <- url_strings
+  files[purrr::map_lgl(files, inherits, "connection")] <- url_files
+
+  if (!all(check_r(files))) {
+    stop(glue::glue("All files must be .R files.",
+                    "\nYou are trying to read the following files:",
+                    "\n * {glue::glue_collapse(file_strings, sep = '\n * ')}"),
+         call. = FALSE
+    )
+  }
+
   if (!all(file.exists(unlist(files)))) {
     stop(glue::glue("The following file(s) do not exist:",
                     "\n * {glue::glue_collapse(files[!file.exists(unlist(files))],
@@ -23,16 +42,37 @@ read_rfiles <- function(...) {
          call. = FALSE
     )
   }
-  d <- purrr::map(files, get_expr)
+  d <- purrr::map2(files, file_strings, get_expr)
   d <- do.call(rbind, d)
   d
 }
 
-get_expr <- function(x) {
+get_expr <- function(x, y) {
   d <- tibble::tibble(
-    file = x,
+    file = y,
     expr = matahari::dance_recital(x, evaluate = FALSE)[['expr']]
   )
   d$line <- 1:nrow(d)
   d
+}
+
+handle_url <- function(x) {
+  on.exit(close(x))
+  open(x, "rb")
+  r <- readLines(x)
+  tmp <- tempfile()
+  path <- glue::glue("{tmp}.R")
+  writeLines(r, path)
+  path
+}
+
+get_url_string <- function(x) {
+  gsub("description \"|\"", "", capture.output(x)[2])
+}
+
+check_r <- function(x) {
+  if (is.list(x)) {
+    purrr::map(x, check_r)
+  }
+  grepl("*.r$", x, ignore.case = TRUE)
 }
--- tests/testthat/test-read_rfiles.R
+++ tests/testthat/test-read_rfiles.R
@@ -9,3 +9,13 @@ test_that("reading R files into tidy data frame works", {
 test_that("reading R files fails if file does not exist", {
   expect_error(read_rfiles("foo.R"), "The following")
 })
+
+test_that("check r files catches non-r files", {
+  expect_false(check_r("test.R.s"))
+  expect_true(check_r("test.R"))
+  expect_true(check_r("test.r"))
+})
+
+test_that("reading R file fails if file is not a .R file", {
+  expect_error(read_rfiles("foo.txt"), "All files must be .R files")
+})
