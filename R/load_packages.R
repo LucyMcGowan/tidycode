309da951de9cca663e8b35a5c02e4765b7b5e795
Lucy McGowan <lucydagostino@gmail.com>
2020-11-01 10:31:05 -0500

    fix :bug:

--- R/load_packages.R
+++ R/load_packages.R
@@ -15,11 +15,11 @@
 #' )
 load_packages <- function(x) {
   pkgs <- ls_packages(x)
-  not_installed <- pkgs[!(pkgs %in% installed.packages()[,"Package"])]
+  not_installed <- pkgs[!(pkgs %in% utils::installed.packages()[,"Package"])]
   if(length(not_installed) > 0) {
     stop_glue("Some of the packages in your call list have not been installed. ",
               "Please install the following package before proceeding:\n",
-              "{glue_collapse(glue('* {not_installed}'), sep = '\n')}")
+              "{glue::glue_collapse(glue::glue('* {not_installed}'), sep = '\n')}")
     } else {
     invisible(lapply(pkgs, require, character.only = TRUE))
   }
